20a944ff7150c30576a1d8e4f64918b4ed4e28f0
ngocht2 <ngocht@blockchainlabs.asia>
2021-12-01 22:48:01 +0700

    add checkbox

---
+++ src/Checkbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="status" type="checkbox" />
+    <div>
+      <slot name="extra"></slot>
+      <label>
+        <slot></slot>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {
+      status: false,
+    };
+  },
+  watch: {
+    status(value) {
+      this.$emit("input", value);
+    },
+  },
+};
+</script>
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Checkbox from "./Checkbox";
+export default {
+  install(Vue) {
+    Vue.component("checkbox", Checkbox);
+  },
+};
+
+export { Checkbox };
